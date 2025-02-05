d4260e562dd93d4557086889b1ab3b2a5ea7ca91
CYBER joe <ahijoe45@gmail.com>
2025-02-05 15:36:38 +0300

    Add files via upload

---
+++ form.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector('.wrapper');
+const loginlink = document.querySelector('.login-link');
+const registerlink = document.querySelector('.register-link');
+const btnpopup = document.querySelector('.btnlogin-popup');
+const iconclose = document.querySelector('.icon-close');
+
+registerlink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+}) 
+
+registerlink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+}) 
+
+btnpopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+ })
+
+ iconclose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+ }) 
+ let subMenu = document.getElementById("subMenu");
+
+ function toggleMenu(){
+    subMenu.classList.toggle("open-menu");
+ }
+ 
